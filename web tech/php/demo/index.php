edb217b6bf3f19f309820665c6a0163d03882485
unknown <mashiatamin08@gmail.com>
2023-02-14 03:48:01 +0600

    php add

---
+++ web tech/php/demo/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+NID number: <input type="text" name="NID"><br>
+E-mail: <input type="text" name="email"><br>
+Birthday:<input type="date" id="birthday" name="birthday"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ web tech/php/demo/welcome.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<ul>
+<li>  <?php echo $_POST["name"]; ?></li>
+<li>  <?php echo $_POST["NID"]; ?></li>
+<li> <?php echo $_POST["email"]; ?> </li>
+<li> <?php echo $_POST["birthday"]; ?> </li>
+</ul>
+
+
+
+
+</body>
+</html>
